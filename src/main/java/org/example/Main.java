d4d9a19dd936b636faf9a2528e7b08246480da5c
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2024-12-24 11:57:47 +0530

    Added:ability to find checpest hotel on date 11Sep2020-12Sep2020

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,13 @@ public class Main
         HotelReservationSystem reservationSystem = new HotelReservationSystem();
 
         reservationSystem.addHotel("Lakewood", 110, 90);
-        reservationSystem.addHotel("Bridgewood", 160, 60);
+        reservationSystem.addHotel("Bridgewood", 150, 50);
         reservationSystem.addHotel("Ridgewood", 220, 150);
 
         //to display
         reservationSystem.displayHotels();
 
-        String result = reservationSystem.findCheapestHotel("10Sep2020", "11Sep2020");
+        String result = reservationSystem.findCheapestHotel("11Sep2020", "12Sep2020");
         System.out.println("Cheapest Hotel: " + result);
     }
 }
